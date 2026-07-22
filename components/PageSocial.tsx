e813481d665a2b8e2ad99893d30561d5b61ed1c1
profjoaonunes <jvitornune@hotmail.com>
2026-07-22 16:56:54 -0300

    Update PageSocial.tsx

--- components/PageSocial.tsx
+++ components/PageSocial.tsx
@@ -24,6 +24,28 @@ const socialLinks: SocialLink[] = [
     )
   },
 
+  config.lattes&&{
+    name: 'lattes',
+    href: 'http://lattes.cnpq.br/1583821044060752',
+    title: 'Currículo Lattes',
+    icon: (
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor'>
+        <path d='M12 3L1 9l11 6 9-4.91V17h2V9L12 3zM5 13.18v4l7 3.82 7-3.82v-4L12 17l-7-3.82z' />
+      </svg>
+    )
+  },
+
+  config.researchgate && {
+    name: 'researchgate',
+    href: 'https://www.researchgate.net/profile/Joao-Vitor-Nunes?ev=hdr_xprf',
+    title: 'ResearchGate',
+    icon: (
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor'>
+        <path d='M19.54 6.74c-.42 0-.82.12-1.15.34l-3.26-1.9a2.53 2.53 0 0 0-2.61-2.62c-1.4 0-2.54 1.14-2.54 2.54 0 .34.07.66.2.96L7.33 8.28a2.53 2.53 0 0 0-2.27-1.41c-1.4 0-2.54 1.14-2.54 2.54s1.14 2.54 2.54 2.54c.94 0 1.76-.52 2.19-1.29l2.88 1.68c-.05.18-.08.37-.08.56 0 1.4 1.14 2.54 2.54 2.54s2.54-1.14 2.54-2.54c0-.36-.08-.71-.22-1.01l3.18-1.85c.35.26.78.42 1.25.42 1.14 0 2.07-.93 2.07-2.07s-.93-2.07-2.07-2.07z'/>
+      </svg>
+    )
+  },
+  
   config.github && {
     name: 'github',
     href: `https://github.com/${config.github}`,
